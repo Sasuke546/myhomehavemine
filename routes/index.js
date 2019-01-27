9a4b22b7bd305bc3bb22095e66e820e1a8e6d163
Sasuke546 <linli8952@gmail.com>
2019-01-27 15:30:31 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -15,11 +15,30 @@ router.get('/get_config',function(req,res){
 		flag: 'r'
 	};
 	var outString;
-	fs.readFile('./share_config.txt', 'utf-8', function(err, data){
-		//outString = data.toString();
+	/*fs.readFile('./share_config.txt', 'utf-8', function(err, data){
+		if(err){
+			throw err;
+		}
+		outString = data.toString();
 		console.log(data);
-	});
-	res.send(outString);
+	});*/
+
+	var data = {forceupdate: 0,  moregame: 1, games: [], floatgameflag : 1,
+		floatgames : [], jumpBox : 1, lockmp : 1, ignorechannels : 'gd001',
+		lockshare : 0, videoTimes: 1, type: '', normal : 4, blacklist: 2,
+		speedUpShare: 0, host: '', shareUrls: '', btns:[], citys:[],
+		shares:[]};
+	res.writeHead(200, {'Content-Type': 'application/json'});
+	res.end(JSON.stringify(data));
+	//res.send('here is the ' + outString);
+});
+
+router.get('loginIn', function(req,res){
+	
+	console.log(req.body);
+
+	
+
 });
 
 module.exports = router;
