fff1db9eb7f4a2f93d5659edf1ffb7cc8eea6364
Sasuke546 <linli8952@gmail.com>
2019-01-27 08:43:53 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var fs = require("fs")
 
 /* GET home page. */
 router.get('/', function(req, res) {
 	var url = req.query.url;
 	var outString = url + "here is the url";
   	res.render('index', { title: outString });
+});
+
+router.get('/get_config',function(req,res){
+	var option = {
+		encoding: 'UTF-8',
+		flag: 'r'
+	};
+	var outString;
+	fs.readFile('./share_config.txt', 'utf-8', function(err, data){
+		//outString = data.toString();
+		console.log(data);
+	});
+	res.send(outString);
 });
 
 module.exports = router;
