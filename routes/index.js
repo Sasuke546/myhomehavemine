6205ae76dfc6a0001f2839e812bf4180a68e2aa3
Sasuke546 <linli8952@gmail.com>
2019-01-27 08:08:08 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express i have mine' });
+	//res.render('index', { title: 'Express i have mine' });
+	res.send('hello');
 });
 
 module.exports = router;
